340b09edd8ae3829727ba405c8c8224170ceb57f
Harendra <harendra.p@gmail.com>
2017-11-17 11:14:07 +0530

    updated

--- Agni-application/src/main/java/com/nttdata/agni/resources/utils/TransformUtils.java
+++ Agni-application/src/main/java/com/nttdata/agni/resources/utils/TransformUtils.java
@@ -3,9 +3,14 @@
  */
 package com.nttdata.agni.resources.utils;
 
+import java.util.List;
+
 import org.hl7.fhir.dstu3.model.Resource;
+import org.hl7.fhir.instance.model.api.IBaseResource;
 
 import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.model.primitive.StringDt;
+import ca.uhn.fhir.util.FhirTerser;
 
 import com.nttdata.agni.resources.core.AbstractResource;
 
@@ -29,5 +34,17 @@ public final class TransformUtils {
         		//.newXmlParser().setPrettyPrint(true)
                 .encodeResourceToString(resource);
 		return encoded;
+	}
+	
+	public static void getFHIRValue(IBaseResource nextResource) {
+		FhirContext ctx = FhirContext.forDstu3();
+		FhirTerser terser = ctx.newTerser();
+		//IBaseResource nextResource = null;
+		List<StringDt> allStringData = terser.getAllPopulatedChildElementsOfType(nextResource, StringDt.class);
+		if (allStringData.size() > 0) {        	
+	    	for (StringDt entity : allStringData) {	    		
+	    		System.out.println(entity.getValueAsString());
+	        }
+    	}
 	}
 }
